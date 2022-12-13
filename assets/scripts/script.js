746472f1127a2f402d19b5463d1d7cce5d7a1065
poetic-potato <nameisseonah@gmail.com>
2022-12-14 00:07:00 +0900

    work home script modify

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,23 @@
 // modal
 function openModal(id) {
   const el = document.getElementById(id);
-  el.classList.add('active');  
+  el.classList.add('active');
+  $(el).trigger('cssChanged');
 }
 function closeModal(id) {
   const el = document.getElementById(id);
-  el.classList.remove('active');  
+  el.classList.remove('active');
+  $(el).trigger('cssChanged');
 }
 
 // bottom-sheet
 function openBottomSheet(id) {
   const el = document.getElementById(id);
-  el.classList.add('active');  
+  el.classList.add('active');
 }
 function closeBottomSheet(id) {
   const el = document.getElementById(id);
-  el.classList.remove('active');  
+  el.classList.remove('active');
 }
 
 // link
@@ -26,7 +28,7 @@ function linkPage(link) {
 // table link
 function openPage(link) {
   window.open(link);
-};
+}
 
 $(document).ready(function () {
   $('table > tbody > tr').click(function () {
